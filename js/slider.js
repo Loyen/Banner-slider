4047f8423dbb753374668a6f1831b3a75fe1db2e
Loyen <patrik.t.freij@gmail.com>
2014-12-29 20:10:13 +0100

    First useable version

--- js/slider.js
+++ js/slider.js
@@ -13,10 +13,10 @@
         slide: '.slide'
       },
       animation: {
-        effect: 'fade',
-        pause: 300,
-        duration: 500,
-        easing: 'linear'
+        pause: 2000,
+        duration: 1000,
+        transition: 'slideInRight',
+        easing: 'easeInOutExpo'
       },
       random: false
     };
@@ -24,8 +24,9 @@
     slider.prototype.data = {
       currentSlide: null,
       lastSlide: null,
-      slides: []
-    }
+      slides: [],
+      animation: { run: null }
+    };
 
     slider.prototype._construct = function(slideIdentifier){
       var 
@@ -41,6 +42,9 @@
       self._initiate();
     };
 
+    /**
+     * Initiate slider
+     */
     slider.prototype._initiate = function(){
       var 
         self = this,
@@ -51,55 +55,100 @@
       for (var i = 0; i < slides.length; i++)
       {
         slides[i].style.display = 'none';
+        slides[i].style.zIndex = 0;
       }
 
       // Set current slide
       data.currentSlide = (options.random ? self.randomSlide() : 0);
 
-      self.newSlide(data.currentSlide);
+      self.newSlide(data.currentSlide, true);
+      self.start();
     };
 
-    slider.prototype.newSlide = function(slideID){
+    /**
+     * Start slider
+     */
+    slider.prototype.start = function(){
+      var 
+        self = this,
+        options = self.options,
+        data = self.data,
+        slides = data.slides;
+
+      data.animation.run = setInterval(function(){
+        var 
+          currentSlideID = data.currentSlide,
+          slideID = (options.random ? self.randomSlide() : currentSlideID+1);
+
+        if (slideID >= slides.length) slideID = 0;
+        
+        self.newSlide(slideID);
+      }, options.animation.pause);
+    };
+
+    /**
+     * Stop slider
+     */
+    slider.prototype.stop = function(){
+      var 
+        self = this,
+        options = self.options,
+        data = self.data;
+
+      clearInterval(data.animation.run);
+    };
+
+    /**
+     * Transition to next slide with/without animation
+     */
+    slider.prototype.newSlide = function(slideID, noAnimation){
       var 
         self = this,
         options = self.options,
         data = self.data,
         slides = data.slides,
-        lastSlide = data.lastSlide,
-        currentSlide = data.currentSlide,
-        sliderWidth = data.slider.offsetWidth,
-        sliderHeight = data.slider.offsetHeight;
-
-      console.log(slideID);
+        lastSlideID = data.lastSlide,
+        lastSlide = slides[lastSlideID],
+        currentSlideID = data.currentSlide,
+        currentSlide = slides[currentSlideID],
+        transition = options.animation.transition;
 
       if (!slides[slideID]) {
-        currentSlide = slides[currentSlide];
+        console.log('Slide '+slideID+' do not exist.');
+      } else {
+        currentSlide = slides[slideID];
+        data.currentSlide = slideID;
+
         currentSlide.style.left = 'auto';
         currentSlide.style.top = 'auto';
         currentSlide.style.bottom = 'auto';
         currentSlide.style.right = 'auto';
-        console.log('sorry bro, no slide');
-        return true;
+
+        currentSlide.style.display = 'block';
+        currentSlide.style.zIndex = 1;
+
+        if (currentSlide.getAttribute('slider-transition')) transition = currentSlide.getAttribute('slider-transition');
+
+        if (lastSlide) lastSlide.style.zIndex = 0;
+
+        // If noAnimation is not set, run transition for current slide
+        if (!noAnimation) {
+          self.stop(); 
+
+          var transitionEffect = self.transition(transition);
+
+          transitionEffect();
+
+          self.start();
+        }
+
+        // Remove old slide if any
+        if (lastSlide) {
+          setTimeout(function(){ lastSlide.style.display = 'none'; }, options.animation.duration);
+        }
+
+        data.lastSlide = data.currentSlide;
       }
-
-      currentSlide = slides[slideID];
-
-      console.log(slideID+' exists and will be animated');
-
-      var easingFunction = self._easing('linear');
-
-      currentSlide.style.left = sliderWidth+'px';
-      currentSlide.style.display = 'block';
-      var offsetLeft = currentSlide.offsetLeft;
-
-      self._animate({
-        run: function(duration, timePassed){
-          var newOffset = offsetLeft-easingFunction(timePassed, 0, offsetLeft, duration)+'px';
-          if (newOffset >= sliderWidth) newOffset = sliderWidth;
-          currentSlide.style.left = newOffset;
-          //currentSlide.style.left = easingFunction(timePassed, offsetLeft, 0, duration)+'px';
-        },
-      });
     };
 
     slider.prototype.randomSlide = function(){
@@ -117,55 +166,253 @@
         return slideID;
     };
 
-    slider.prototype._click = function(item, e){
+    slider.prototype.click = function(item, e){
     };
 
-    slider.prototype._animate = function(obj){
+    slider.prototype.transition = function(transition, easing){
+      var 
+        self = this,
+        options = self.options,
+        data = self.data,
+        slides = data.slides,
+        lastSlideID = data.lastSlide,
+        lastSlide = slides[lastSlideID],
+        currentSlideID = data.currentSlide,
+        currentSlide = slides[currentSlideID],
+        sliderWidth = data.slider.offsetWidth,
+        sliderHeight = data.slider.offsetHeight,
+        transition = transition || options.animation.transition,
+        easing = easing || options.animation.easing,
+        tweenFunction = self._tween(easing);
+
+      if (transition == 'slideInVertical') {
+        transition = (currentSlideID > lastSlideID ? 'slideInTop' : 'slideInBottom');
+      } else if (transition == 'slideInVerticalReversed') {
+        transition = (currentSlideID < lastSlideID ? 'slideInTop' : 'slideInBottom');
+      } else if (transition == 'slideInHorizontal') {
+        transition = (currentSlideID > lastSlideID ? 'slideInRight' : 'slideInLeft');
+      } else if (transition == 'slideInHorizontalReversed') {
+        transition = (currentSlideID < lastSlideID ? 'slideInRight' : 'slideInLeft');
+      }
+
+      return {
+        fadeIn: function(){
+          currentSlide.style.opacity = 0;
+
+          self.animate({
+            run: function(duration, timePassed){
+              var newOpacity = tweenFunction(timePassed, 0, 1, duration);
+              if (newOpacity >= 1) newOpacity = 1;
+              currentSlide.style.opacity = newOpacity;
+            },
+            complete: function(){ currentSlide.style.opacity = 1; }
+          });
+        },
+        slideInTop: function(){
+          currentSlide.style.top = '-'+sliderWidth+'px';
+          var offsetTop = currentSlide.offsetTop;
+
+          self.animate({
+            run: function(duration, timePassed){
+              var newOffset = offsetTop-tweenFunction(timePassed, 0, offsetTop, duration)+'px';
+              if (newOffset >= sliderHeight) newOffset = sliderHeight;
+              currentSlide.style.top = newOffset;
+            },
+            complete: function(){ currentSlide.style.top = 0; }
+          });
+        },
+        slideInRight: function(){
+          currentSlide.style.right = sliderWidth+'px';
+          var offsetLeft = currentSlide.offsetLeft;
+
+          self.animate({
+            run: function(duration, timePassed){
+              var newOffset = offsetLeft-tweenFunction(timePassed, 0, offsetLeft, duration)+'px';
+              if (newOffset >= sliderWidth) newOffset = sliderWidth;
+              currentSlide.style.right = newOffset;
+            },
+            complete: function(){ currentSlide.style.right = 0; }
+          });
+        },
+        slideInBottom: function(){
+          currentSlide.style.bottom = sliderWidth+'px';
+          var offsetTop = currentSlide.offsetTop;
+
+          self.animate({
+            run: function(duration, timePassed){
+              var newOffset = offsetTop-tweenFunction(timePassed, 0, offsetTop, duration)+'px';
+              if (newOffset >= sliderHeight) newOffset = sliderHeight;
+              currentSlide.style.bottom = newOffset;
+            },
+            complete: function(){ currentSlide.style.bottom = 0; }
+          });
+        },
+        slideInLeft: function(){
+          currentSlide.style.left = '-'+sliderWidth+'px';
+          var offsetLeft = currentSlide.offsetLeft;
+
+          self.animate({
+            run: function(duration, timePassed){
+              var newOffset = offsetLeft-tweenFunction(timePassed, 0, offsetLeft, duration)+'px';
+              if (newOffset >= sliderWidth) newOffset = sliderWidth;
+              currentSlide.style.left = newOffset;
+            },
+            complete: function(){ currentSlide.style.left = 0; }
+          });
+        }
+      }[transition];
+    };
+
+    slider.prototype.animate = function(obj){
       var 
         self = this,
         options = self.options,
         duration = obj.duration || options.animation.duration,
-        timeStart = new Date().getTime(),
+        timeStart = new Date().getTime();
 
-      animate = setInterval(function(){
+      var animate = setInterval(function(){
         var timePassed = new Date().getTime() - timeStart;
+
+        if (timePassed >= duration) timePassed = duration;
 
         // Run animation, log if none set
         if (obj.run) obj.run(duration, timePassed);
-        else console.log('No animation set.', 'warning');
-
-        console.log(timePassed + ' ' + duration);
 
         if (timePassed >= duration){
           clearInterval(animate);
 
           // Do complete function 
           if (obj.complete) obj.complete();
+
+          console.log('Animation complete');
         }
       },0);
     };
 
-    slider.prototype._easing = function(easing){
-      var self = this;
+    slider.prototype._tween = function(type){
       return {
-        linear: function(t, b, c, d){
-          return c * (t / d) + b;
+        /* Credit to Robert Penner @ http://gizma.com/easing */
+        // simple linear tweening - no easing, no acceleration
+        linear: function (t, b, c, d) {
+          return c*t/d + b;
         },
-        easeIn: function(t, b, c, d){
-          return c*Math.pow(t / d, 1);
+        // quadratic easing in - accelerating from zero velocity
+        easeInQuad: function (t, b, c, d) {
+          t /= d;
+          return c*t*t + b;
         },
-        easeOut: function(t, b, c, d){
-          return c*(1-Math.pow(1 - (t / d), 1));
+        // quadratic easing out - decelerating to zero velocity
+        easeOutQuad: function (t, b, c, d) {
+          t /= d;
+          return -c * t*(t-2) + b;
+        },
+        // quadratic easing in/out - acceleration until halfway, then deceleration
+        easeInOutQuad: function (t, b, c, d) {
+          t /= d/2;
+          if (t < 1) return c/2*t*t + b;
+          t--;
+          return -c/2 * (t*(t-2) - 1) + b;
+        },
+        // cubic easing in - accelerating from zero velocity
+        easeInCubic: function (t, b, c, d) {
+          t /= d;
+          return c*t*t*t + b;
+        },
+        // cubic easing out - decelerating to zero velocity
+        easeOutCubic: function (t, b, c, d) {
+          t /= d;
+          t--;
+          return c*(t*t*t + 1) + b;
+        },
+        // cubic easing in/out - acceleration until halfway, then deceleration
+        easeInOutCubic: function (t, b, c, d) {
+          t /= d/2;
+          if (t < 1) return c/2*t*t*t + b;
+          t -= 2;
+          return c/2*(t*t*t + 2) + b;
+        },
+        // quartic easing in - accelerating from zero velocity
+        easeInQuart: function (t, b, c, d) {
+          t /= d;
+          return c*t*t*t*t + b;
+        },
+        // quartic easing out - decelerating to zero velocity
+        easeOutQuart: function (t, b, c, d) {
+          t /= d;
+          t--;
+          return -c * (t*t*t*t - 1) + b;
+        },
+        // quartic easing in/out - acceleration until halfway, then deceleration
+        easeInOutQuart: function (t, b, c, d) {
+          t /= d/2;
+          if (t < 1) return c/2*t*t*t*t + b;
+          t -= 2;
+          return -c/2 * (t*t*t*t - 2) + b;
+        },
+        // quintic easing in - accelerating from zero velocity
+        easeInQuint: function (t, b, c, d) {
+          t /= d;
+          return c*t*t*t*t*t + b;
+        },
+        // quintic easing out - decelerating to zero velocity
+        easeOutQuint: function (t, b, c, d) {
+          t /= d;
+          t--;
+          return c*(t*t*t*t*t + 1) + b;
+        },
+        // quintic easing in/out - acceleration until halfway, then deceleration
+        easeInOutQuint: function (t, b, c, d) {
+          t /= d/2;
+          if (t < 1) return c/2*t*t*t*t*t + b;
+          t -= 2;
+          return c/2*(t*t*t*t*t + 2) + b;
+        },
+        // sinusoidal easing in - accelerating from zero velocity
+        easeInSine: function (t, b, c, d) {
+          return -c * Math.cos(t/d * (PI/2)) + c + b;
+        },
+        // sinusoidal easing out - decelerating to zero velocity
+        easeOutSine: function (t, b, c, d) {
+          return c * Math.sin(t/d * (PI/2)) + b;
+        },
+        // sinusoidal easing in/out - accelerating until halfway, then decelerating
+        easeInOutSine: function (t, b, c, d) {
+          return -c/2 * (Math.cos(PI*t/d) - 1) + b;
+        },
+        // exponential easing in - accelerating from zero velocity
+        easeInExpo: function (t, b, c, d) {
+          return c * Math.pow( 2, 10 * (t/d - 1) ) + b;
+        },
+        // exponential easing out - decelerating to zero velocity
+        easeOutExpo: function (t, b, c, d) {
+          return c * ( -Math.pow( 2, -10 * t/d ) + 1 ) + b;
+        },
+        // exponential easing in/out - accelerating until halfway, then decelerating
+        easeInOutExpo: function (t, b, c, d) {
+          t /= d/2;
+          if (t < 1) return c/2 * Math.pow( 2, 10 * (t - 1) ) + b;
+          t--;
+          return c/2 * ( -Math.pow( 2, -10 * t) + 2 ) + b;
+        },
+        // circular easing in - accelerating from zero velocity
+        easeInCirc: function (t, b, c, d) {
+          t /= d;
+          return -c * (Math.sqrt(1 - t*t) - 1) + b;
+        },
+        // circular easing out - decelerating to zero velocity
+        easeOutCirc: function (t, b, c, d) {
+          t /= d;
+          t--;
+          return c * Math.sqrt(1 - t*t) + b;
+        },
+        // circular easing in/out - acceleration until halfway, then deceleration
+        easeInOutCirc: function (t, b, c, d) {
+          t /= d/2;
+          if (t < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+          t -= 2;
+          return c/2 * (Math.sqrt(1 - t*t) + 1) + b;
         }
-      }[easing];
-    };
-
-    slider.prototype.log = function(message, type){
-      if (!type) type = 'notice';
-
-      type = type.toUpperCase();
-
-      console.log(message);
+      }[type];
     };
 
     return slider;
